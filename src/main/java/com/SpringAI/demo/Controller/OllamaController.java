58bc642dbf3f782f83df868a14ee4f3d2eb78896
MahdiIder <mahdiider@gmail.com>
2026-07-21 02:58:39 +0330

    create sipmle chatmodel

---
+++ src/main/java/com/SpringAI/demo/Controller/OllamaController.java
@@ -0,0 +1,26 @@
+package com.SpringAI.demo.Controller;
+
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("ollama")
+public class OllamaController {
+
+    @Autowired
+    private ChatClient chatClient;
+
+
+    @GetMapping("api/{message}")
+    public String getAnswer(@PathVariable String message){
+
+        return chatClient.prompt(message).call().content();
+
+    }
+
+}
---
+++ src/main/java/com/SpringAI/demo/config/AppConfig.java
@@ -0,0 +1,16 @@
+package com.SpringAI.demo.config;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    public ChatClient chatClient(ChatModel chatModel){
+        return ChatClient.create(chatModel);
+    }
+
+}
